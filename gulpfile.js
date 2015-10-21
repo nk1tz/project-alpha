34194228d23a15abf8a8cd09cbd169c53dc9146e
Nathaniel Kitzke <nat.kitzke@gmail.com>
2015-10-21 16:54:58 +0000

    fresh state

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
+'use strict';
 var gulp = require('gulp');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
 
 gulp.task('browserify', function() {
    browserify('./src/js/main.js')
@@ -18,6 +20,12 @@ gulp.task('copy', function() {
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['browserify', 'copy'], function() {
+gulp.task('sass', function () {
+  gulp.src('./src/scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('default', ['browserify', 'sass', 'copy'], function() {
     return gulp.watch('src/**/*.*', ['browserify', 'copy']);
 });
